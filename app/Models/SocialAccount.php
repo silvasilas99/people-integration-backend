1cd3466a815eb6ba635f4c2b56080a09d3cdf67f
Silas Santos da Silva <silvasilas66@gmail.com>
2022-01-07 19:30:41 -0300

    Creating SocialAccount model and migration

---
+++ app/Models/SocialAccount.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+
+class SocialAccount extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'social_code',
+        'social_provider',
+        'social_name',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
